8cb12065c1c7b06de67998db6f85e250ae3e52b4
Lorenzo Guzzetti <lguzz499@gmail.com>
2023-05-29 12:28:50 +0200

    °movement°

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    private Camera mainCam;
+    private Vector3 mousePos;
+    private PlayerMovement player;
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canFire;
+    private float timer;
+    public float timeBetweenFiring;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rotation = mousePos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        if (player.IsFacingRight)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        } 
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, rotZ + 180);
+        }
+
+        if(!canFire)
+        {
+            timer += Time.deltaTime;
+            if(timer > timeBetweenFiring)
+            {
+                canFire = true;
+                timer = 0;
+            }
+        }
+
+        if(Input.GetMouseButton(0) && canFire)
+        {
+            canFire = false;
+            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+        }
+    }
+}
